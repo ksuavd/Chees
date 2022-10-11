59063d7e86f3b1cfc830aa88d18c4694b98f4110
Avdee_nw28 <Avdee_nw28@itstepru.onmicrosoft.com>
2022-10-11 21:12:12 +0500

    Добавьте файлы проекта.

---
+++ Chees/Chees.cpp
@@ -0,0 +1,34 @@
+﻿// Chees.cpp 
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "");
+    int size;
+    cout << "Введите размер: "; cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            for (int k = 0; k < size; k++)
+            {
+                for (int l = 0; l < size; l++)
+                {
+                    if ((i + k) % 2) cout << "  ";
+                    else cout << "* ";
+
+                }
+            }
+            cout << endl;
+        }
+    }
+    
+}
+
+ 
+
+
+
+
+
